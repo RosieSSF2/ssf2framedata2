fd46213f36e78bdf0ce557adfb5cd1defe005398
JustSomeGuy2295 <jesasx775@gmail.com>
2025-12-27 09:31:57 +1000

    Sora fully added
    - Added all images
    - Changed bot description
    - Added sora to hitboxes.py

--- cogs/hitboxes.py
+++ cogs/hitboxes.py
@@ -507,6 +507,23 @@ class Hitboxes(commands.Cog):
         ssf2_embed, view = ssf2_hitbox('Sonic', attack, interaction.user)
         await interaction.response.send_message(embed=ssf2_embed[0], view=view)
 
+    # Sora
+    moves = Literal[
+        'Jab', 'Dash Attack',
+        'Down Tilt', 'Up Tilt', 'Forward Tilt',
+        'Neutral Air', 'Down Air', 'Up Air', 'Forward Air', 'Back Air',
+        'Down Smash', 'Up Smash', 'Forward Smash', 
+        'Up Special', 'Neutral Special',
+        'Down Special', 'Side Special',
+        'Grab', 'Forward Throw', 'Back Throw', 'Up Throw', 'Down Throw'
+    ]   
+    
+    @app_commands.command(name='sora')
+    async def sora(self, interaction: discord.Interaction, attack: moves):
+        """Sora frame data and hitbox info"""
+        ssf2_embed, view = ssf2_hitbox('Sora', attack, interaction.user)
+        await interaction.response.send_message(embed=ssf2_embed[0], view=view)
+
     # Waluigi
     moves = Literal[
         'Jab', 'Dash Attack',
